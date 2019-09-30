1dc744a9eb572fbc092056681c6523d7ba76ee10
Panos Petropoulos <icsd13152@icsd.aegean.gr>
2019-09-30 16:49:07 +0300

    First commit

---
+++ src/Snake.java
@@ -0,0 +1,36 @@
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author p.petropoulos
+ */
+public class Snake extends JFrame {
+
+    public Snake() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Table());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+
+}
